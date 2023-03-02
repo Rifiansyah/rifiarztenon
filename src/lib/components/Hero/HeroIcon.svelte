ef42d985d56192b5f03105b6d5f2d3279ce849d5
gneiru <noelrohi59@gmail.com>
2023-03-02 22:37:53 +0800

    fix icon badge

--- src/lib/components/Hero/HeroIcon.svelte
+++ src/lib/components/Hero/HeroIcon.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
 	import { Avatar } from "@skeletonlabs/skeleton";
+	import Dark from '$elements/dark.png'
+	import Light from '$elements/light.png'
+	import Basic from '$elements/basic.png'
+	import Fire from '$elements/fire.png'
+	import Water from '$elements/water.png'
+	import Earth from '$elements/earth.png'
     export let icon: string
 	export let name: string
 	export let element: string
+
+	let elementIcon = {
+		dark: Dark,
+		basic: Basic,
+		light: Light,
+		water: Water,
+		earth: Earth,
+		fire: Fire,
+	}[element] || Basic
 </script>
 
 <div class="relative inline-block">
 	<span class="badge-icon variant-filled-primary absolute -top-1 -right-1 z-10"
-		><img class="rounded-3xl" src="\src\lib\images\elements\{element.toLowerCase()}.png" alt="{name}'s element" /></span
+		><img class="rounded-3xl" src={elementIcon} alt="{name}'s element" /></span
 	>
 	<Avatar src={icon} initials={name} />
 </div>
