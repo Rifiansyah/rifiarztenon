d4099ad41f5024b6fb88d2f982c08ee848091624
gneiru <noelrohi59@gmail.com>
2023-03-18 08:42:53 +0800

    feat: landing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
@@ -6,21 +6,17 @@
 	import Title from '$lib/components/Title.svelte';
 </script>
 
-<!-- App Shell -->
 <Title />
 <AppShell>
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-center">
+		<AppBar gridColumns="grid-cols-3" slotTrail="place-content-end">
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">GuardiTa</strong>
+				<strong class="text-xl uppercase"><a href="/" class="btn">Guardita</a></strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a href="/hero">Hero</a>
-				<LightSwitch />
+				<LightSwitch class="mr-4" />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
 	<slot />
 </AppShell>
